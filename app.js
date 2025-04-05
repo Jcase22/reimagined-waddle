022425800dd0b41e4044baf878f5b5310a971171
Justin Case <justin.case1061@gmail.com>
2025-04-05 14:59:42 -0500

    product page routing complete

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import userRouter from './routers/UserRouter.js';
 import productRouter from './routers/ProductRouter.js';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(express.static('views'));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/product/details/:productId', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'product/index.html'));
+})
 
 app.use('/user', userRouter)
 app.use('/products', productRouter)
